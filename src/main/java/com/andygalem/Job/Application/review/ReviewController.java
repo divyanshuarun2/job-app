4edd66bf2a88df0e89b397f5ab2a9d4239f439ed
divyanshuarun2 <divyanshuarun2@gmail.com>
2024-09-20 00:58:02 +0530

    Starting on Reviews

---
+++ src/main/java/com/andygalem/Job/Application/review/ReviewController.java
@@ -0,0 +1,10 @@
+package com.andygalem.Job.Application.review;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/company/reviews")
+public class ReviewController {
+
+}
