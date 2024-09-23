d42147559aad16d005d608ed6d687aac8221ba71
divyanshuarun2 <divyanshuarun2@gmail.com>
2024-09-23 15:46:53 +0530

    fixed the ReviewController

--- src/main/java/com/andygalem/Job/Application/review/ReviewController.java
+++ src/main/java/com/andygalem/Job/Application/review/ReviewController.java
@@ -3,12 +3,6 @@ package com.andygalem.Job.Application.review;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-@Controller
-@RequestMapping("/company/reviews")
-public class ReviewController {
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
